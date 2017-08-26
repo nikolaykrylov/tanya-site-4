62165eaec3ea12d4aadebb644c5125b4ac394524
Nikolay <nnkrylov@gmail.com>
2017-08-26 20:13:06 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $(".main_mnu_button").click(function() {
+        $(".maian_mnu ul").slideToggle();
+    });
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+	sass = require('gulp-sass');
+	browserSync = require('browser-sync');
+
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+});
